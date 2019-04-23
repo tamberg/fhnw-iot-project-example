5fd3beffe3440a0aab59447d97a371916f334b4a
tamberg <tamberg@yaler.net>
2019-04-23 15:07:53 +0200

    Added for loop.

--- Arduino/ESP8266_Display_Test/ESP8266_Display_Test.ino
+++ Arduino/ESP8266_Display_Test/ESP8266_Display_Test.ino
@@ -8,8 +8,20 @@ TM1637 tm1637(CLK,DIO);
 void setup() {
   tm1637.set();
   tm1637.init();
-  int8_t displayBytes[] = { 0x01, 0x02, 0x03, 0x04 };
-  tm1637.display(displayBytes);
 }
 
-void loop() {}
+void displayBytes(int8_t *bytes) {
+  tm1637.display(bytes);
+  delay(500);
+}
+
+void loop() {
+  int8_t b[] = { 0x00, 0x00, 0x00, 0x00 };
+  for (int i = 0; i < 255; i++) {
+    b[0] = i;
+    b[1] = i % 1000 / 100;
+    b[2] = i % 100 / 10;
+    b[3] = i % 10;
+    displayBytes(b);
+  }
+}
